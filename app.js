09afacc70b302ef354331d415afed10a82851e71
viery365 <20521892+viery365@users.noreply.github.com>
2017-10-26 10:34:50 +0100

    Add local variables

--- app.js
+++ app.js
@@ -3,20 +3,19 @@ var scores, roundScore, activePlayer, gamePlaying;
 init();
 
 document.getElementById("btn-roll").addEventListener("click", function() {
+  var doc = document;
+  var diceDom = doc.getElementById("dice");
   if (gamePlaying) {
     //1. Random Number
     var dice = Math.floor(Math.random() * 6) + 1;
     //2. Display the result
-    var diceDom = document.getElementById("dice");
     diceDom.style.display = "block";
     diceDom.src = "img/dice-" + dice + ".png";
 
     //3. Update the round score but only if the rolled number was not a 1
     if (dice !== 1) {
       roundScore += dice;
-      document.getElementById(
-        "current-" + activePlayer
-      ).textContent = roundScore;
+      doc.getElementById("current-" + activePlayer).textContent = roundScore;
     } else {
       nextPlayer();
     }
@@ -24,22 +23,23 @@ document.getElementById("btn-roll").addEventListener("click", function() {
 });
 
 document.getElementById("btn-hold").addEventListener("click", function() {
+  var doc = document;
   if (gamePlaying) {
     //Add current score to the global score
     scores[activePlayer] += roundScore;
 
     //Update the UI
-    document.getElementById("score-" + activePlayer).textContent =
+    doc.getElementById("score-" + activePlayer).textContent =
       scores[activePlayer];
 
     //Check if player won the game
     if (scores[activePlayer] >= 100) {
-      document.getElementById("name-" + activePlayer).textContent = "Winner!";
-      document.getElementById("dice").style.display = "none";
-      document
+      doc.getElementById("name-" + activePlayer).textContent = "Winner!";
+      doc.getElementById("dice").style.display = "none";
+      doc
         .getElementById("player-" + activePlayer + "-panel")
         .classList.add("winner");
-      document
+      doc
         .getElementById("player-" + activePlayer + "-panel")
         .classList.remove("active");
       gamePlaying = false;
@@ -50,15 +50,16 @@ document.getElementById("btn-hold").addEventListener("click", function() {
 });
 
 function nextPlayer() {
+  var doc = document;
   activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
   roundScore = 0;
-  document.getElementById("current-0").textContent = "0";
-  document.getElementById("current-1").textContent = "0";
+  doc.getElementById("current-0").textContent = "0";
+  doc.getElementById("current-1").textContent = "0";
 
-  document.getElementById("player-0-panel").classList.toggle("active");
-  document.getElementById("player-1-panel").classList.toggle("active");
+  doc.getElementById("player-0-panel").classList.toggle("active");
+  doc.getElementById("player-1-panel").classList.toggle("active");
 
-  document.getElementById("dice").style.display = "none";
+  doc.getElementById("dice").style.display = "none";
 }
 
 document.getElementById("btn-new").addEventListener("click", init);
@@ -68,17 +69,20 @@ function init() {
   activePlayer = 0;
   roundScore = 0;
   gamePlaying = true;
+  var doc = document;
+  var player0Panel = doc.getElementById("player-0-panel");
+  var player1Panel = doc.getElementById("player-1-panel");
 
-  document.getElementById("dice").style.display = "none";
-  document.getElementById("score-0").textContent = "0";
-  document.getElementById("score-1").textContent = "0";
-  document.getElementById("current-0").textContent = "0";
-  document.getElementById("current-1").textContent = "0";
-  document.getElementById("name-0").textContent = "Player 1";
-  document.getElementById("name-1").textContent = "Player 2";
-  document.getElementById("player-0-panel").classList.remove("winner");
-  document.getElementById("player-1-panel").classList.remove("winner");
-  document.getElementById("player-0-panel").classList.remove("active");
-  document.getElementById("player-1-panel").classList.remove("active");
-  document.getElementById("player-0-panel").classList.add("active");
+  doc.getElementById("dice").style.display = "none";
+  doc.getElementById("score-0").textContent = "0";
+  doc.getElementById("score-1").textContent = "0";
+  doc.getElementById("current-0").textContent = "0";
+  doc.getElementById("current-1").textContent = "0";
+  doc.getElementById("name-0").textContent = "Player 1";
+  doc.getElementById("name-1").textContent = "Player 2";
+  player0Panel.classList.remove("winner");
+  player1Panel.classList.remove("winner");
+  player0Panel.classList.remove("active");
+  player1Panel.classList.remove("active");
+  player0Panel.classList.add("active");
 }
