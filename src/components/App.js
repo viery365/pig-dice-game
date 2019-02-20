4b302498c069ef74aae102ec17ed5ea173c62f9f
viery365 <20521892+viery365@users.noreply.github.com>
2019-02-20 06:25:28 +0000

    Add basic JavaScript setup

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,26 @@ function Reset() {
   );
 }
 
+function PlayerPanel(props) {
+  const { id } = props;
+  return (
+    <div id={`player-${id}-panel`} className={props.active}>
+      <div className="player-name" id={`name-${id}`}>
+        Player {id + 1}
+      </div>
+      <div className="player-score" id={`score-${id}`}>
+        {props.score}
+      </div>
+      <div className="player-current-box">
+        <div className="player-current-label">Current</div>
+        <div className="player-current-score" id={`current-${id}`}>
+          {props.current}
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function Roll() {
   return (
     <button id="btn-roll">
@@ -38,45 +58,40 @@ function Controllers() {
   );
 }
 
-function PlayerPanel(props) {
-  const { id } = props;
-  return (
-    <div id={`player-${id}-panel`} className={props.active}>
-      <div className="player-name" id={`name-${id}`}>
-        Player {id + 1}
-      </div>
-      <div className="player-score" id={`score-${id}`}>
-        {props.score}
-      </div>
-      <div className="player-current-box">
-        <div className="player-current-label">Current</div>
-        <div className="player-current-score" id={`current-${id}`}>
-          {props.current}
-        </div>
-      </div>
-    </div>
-  );
+function Dice() {
+  return <img src={dice5} alt="Dice" id="dice" />;
 }
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     playerOneactive: true,
-  //     playerTwoActive: false,
-  //   };
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerOneScore: 0,
+      playerTwoScore: 0,
+      activePlayer: 1,
+      currentScore: 0,
+      gamePlaying: true,
+    };
+  }
 
   render() {
     return (
       <div id="wrapper" className="clearfix">
         <Reset />
-        <PlayerPanel id={0} active="active" score={0} current={0} />
-        <PlayerPanel id={1} active="" score={0} current={0} />
-
+        <PlayerPanel
+          id={0}
+          active={this.state.activePlayer === 1 ? 'active' : ''}
+          score={this.state.playerOneScore}
+          current={this.state.currentScore}
+        />
+        <PlayerPanel
+          id={1}
+          active={this.state.activePlayer === 2 ? 'active' : ''}
+          score={this.state.playerTwoScore}
+          current={this.state.currentScore}
+        />
         <Controllers />
-
-        {/* <img src={dice5} alt="Dice" id="dice" /> */}
+        <Dice />
       </div>
     );
   }
