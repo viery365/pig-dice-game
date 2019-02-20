540da986a8331f027d54aeac6d225a39604347eb
viery365 <20521892+viery365@users.noreply.github.com>
2019-02-20 05:19:15 +0000

    Make react components out of the HTML

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,82 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from 'react';
 
-import dice5 from "./../img/dice-5.png";
+import dice5 from '../img/dice-5.png';
+
+function Reset() {
+  return (
+    <button id="btn-new">
+      <i className="ion-ios-plus-outline" />
+      New game
+    </button>
+  );
+}
+
+function Roll() {
+  return (
+    <button id="btn-roll">
+      <i className="ion-ios-loop" />
+      Roll dice
+    </button>
+  );
+}
+
+function Hold() {
+  return (
+    <button id="btn-hold">
+      <i className="ion-ios-download-outline" />
+      Hold
+    </button>
+  );
+}
+
+function Controllers() {
+  return (
+    <Fragment>
+      <Roll />
+      <Hold />
+    </Fragment>
+  );
+}
+
+function PlayerPanel(props) {
+  const { id } = props;
+  return (
+    <div id={`player-${id}-panel`} className={props.active}>
+      <div className="player-name" id={`name-${id}`}>
+        Player {id + 1}
+      </div>
+      <div className="player-score" id={`score-${id}`}>
+        {props.score}
+      </div>
+      <div className="player-current-box">
+        <div className="player-current-label">Current</div>
+        <div className="player-current-score" id={`current-${id}`}>
+          {props.current}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     playerOneactive: true,
+  //     playerTwoActive: false,
+  //   };
+  // }
+
   render() {
     return (
-      <div className="App">
-        <div id="wrapper" className="clearfix">
-          <div id="player-0-panel" className="active">
-            <div className="player-name" id="name-0">
-              Player 1
-            </div>
-            <div className="player-score" id="score-0">
-              43
-            </div>
-            <div className="player-current-box">
-              <div className="player-current-label">Current</div>
-              <div className="player-current-score" id="current-0">
-                11
-              </div>
-            </div>
-          </div>
+      <div id="wrapper" className="clearfix">
+        <Reset />
+        <PlayerPanel id={0} active="active" score={0} current={0} />
+        <PlayerPanel id={1} active="" score={0} current={0} />
 
-          <div id="player-1-panel">
-            <div className="player-name" id="name-1">
-              Player 2
-            </div>
-            <div className="player-score" id="score-1">
-              72
-            </div>
-            <div className="player-current-box">
-              <div className="player-current-label">Current</div>
-              <div className="player-current-score" id="current-1">
-                0
-              </div>
-            </div>
-          </div>
+        <Controllers />
 
-          <button id="btn-new">
-            <i className="ion-ios-plus-outline" />
-            New game
-          </button>
-          <button id="btn-roll">
-            <i className="ion-ios-loop" />
-            Roll dice
-          </button>
-          <button id="btn-hold">
-            <i className="ion-ios-download-outline" />
-            Hold
-          </button>
-
-          <img src={dice5} alt="Dice" id="dice" />
-        </div>
+        {/* <img src={dice5} alt="Dice" id="dice" /> */}
       </div>
     );
   }
