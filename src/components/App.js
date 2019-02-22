8ef710bbf36c97249954e8cc55ff4baa6739bbd1
viery365 <20521892+viery365@users.noreply.github.com>
2019-02-22 03:45:53 +0000

    Add nextPlayer() method
    Unlike the original version, when the dice marks 1, it's still shown in the view

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import dice1 from "../img/dice-1.png";
+import dice2 from "../img/dice-2.png";
+import dice3 from "../img/dice-3.png";
+import dice4 from "../img/dice-4.png";
+import dice5 from "../img/dice-5.png";
+import dice6 from "../img/dice-6.png";
 
-import dice5 from '../img/dice-5.png';
+const dice = [];
 
-function Reset() {
-  return (
-    <button id="btn-new">
-      <i className="ion-ios-plus-outline" />
-      New game
-    </button>
-  );
-}
+dice.push(dice1, dice2, dice3, dice4, dice5, dice6);
 
 function PlayerPanel(props) {
   const { id } = props;
@@ -36,7 +35,7 @@ PlayerPanel.propTypes = {
   id: PropTypes.number.isRequired,
   active: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
-  current: PropTypes.number.isRequired,
+  current: PropTypes.number.isRequired
 };
 
 function Controllers(props) {
@@ -55,12 +54,8 @@ function Controllers(props) {
 }
 
 Controllers.propTypes = {
-  onRoll: PropTypes.func.isRequired,
+  onRoll: PropTypes.func.isRequired
 };
-
-function Dice() {
-  return <img src={dice5} alt="Dice" id="dice" />;
-}
 
 class App extends Component {
   constructor(props) {
@@ -68,38 +63,76 @@ class App extends Component {
     this.state = {
       playerOneScore: 0,
       playerTwoScore: 0,
-      activePlayer: 0,
-      currentScore: 0,
+      activePlayer: 1,
+      playerOneCurrentScore: 0,
+      playerTwoCurrentScore: 0,
       gamePlaying: true,
     };
 
     this.rollDice = this.rollDice.bind(this);
   }
 
+  nextPlayer(diceNumber) {
+    this.setState(() => ({
+      diceNumber,
+      activePlayer: this.state.activePlayer === 1 ? 2 : 1,
+      playerOneCurrentScore: this.state.playerOneCurrentScore && 0,
+      playerTwoCurrentScore: this.state.playerTwoCurrentScore && 0,
+    }));
+  }
+
+  setCurrentScore(diceNumber) {
+    if (this.state.activePlayer === 1) {
+      return this.setState(() => ({
+        diceNumber,
+        playerOneCurrentScore: this.state.playerOneCurrentScore + diceNumber
+      }));
+    }
+    return this.setState(() => ({
+      diceNumber,
+      playerTwoCurrentScore: this.state.playerTwoCurrentScore + diceNumber
+    }));
+  }
+
   rollDice() {
     if (this.state.gamePlaying) {
-      console.log('test');
+      const diceNumber = Math.floor(Math.random() * 6) + 1;
+      if (diceNumber !== 1) {
+        return this.setCurrentScore(diceNumber);
+      }
+      return this.nextPlayer(diceNumber);
     }
+    return null;
   }
 
   render() {
     return (
       <div id="wrapper" className="clearfix">
-        <Reset />
+        <button id="btn-new">
+          <i className="ion-ios-plus-outline" />
+          New game
+        </button>
         <PlayerPanel
           id={0}
-          active={!this.state.activePlayer ? 'active' : ''}
+          active={this.state.activePlayer === 1 ? "active" : ""}
           score={this.state.playerOneScore}
-          current={this.state.currentScore}
+          current={this.state.playerOneCurrentScore}
         />
         <PlayerPanel
           id={1}
-          active={this.state.activePlayer ? 'active' : ''}
+          active={this.state.activePlayer === 2 ? "active" : ""}
           score={this.state.playerTwoScore}
-          current={this.state.currentScore}
+          current={this.state.playerTwoCurrentScore}
         />
         <Controllers onRoll={this.rollDice} />
-        <Dice />
+        {this.state.diceNumber && (
+          <img
+            src={dice[this.state.diceNumber - 1]}
+            alt="Dice"
+            id="dice"
+            style={{ display: "block" }}
+          />
+        )}
       </div>
     );
   }
