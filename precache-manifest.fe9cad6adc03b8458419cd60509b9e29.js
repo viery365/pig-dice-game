7e1a5d9cca585e656b625c67c03cdbc72bbf2bf3
viery365 <20521892+viery365@users.noreply.github.com>
2019-02-27 13:05:15 +0000

    Updates

---
+++ precache-manifest.fe9cad6adc03b8458419cd60509b9e29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5863f04db5cd8dcb4ab5",
+    "url": "/pig-dice-game/static/css/main.eba15059.chunk.css"
+  },
+  {
+    "revision": "5863f04db5cd8dcb4ab5",
+    "url": "/pig-dice-game/static/js/main.5863f04d.chunk.js"
+  },
+  {
+    "revision": "b515f276200df7200852",
+    "url": "/pig-dice-game/static/js/runtime~main.b515f276.js"
+  },
+  {
+    "revision": "27411e3d3a17a7fb2935",
+    "url": "/pig-dice-game/static/js/2.27411e3d.chunk.js"
+  },
+  {
+    "revision": "c596927b7657434ffe1984b8ff194b71",
+    "url": "/pig-dice-game/static/media/back.c596927b.jpg"
+  },
+  {
+    "revision": "700eaea59b9247dcb46b566936d2cddd",
+    "url": "/pig-dice-game/index.html"
+  }
+];
